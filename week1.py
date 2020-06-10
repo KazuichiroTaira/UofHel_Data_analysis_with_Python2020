79638ad7600bf1fd05b98cd90195b6359d81d090
KazuichiroTaira <ktaira098@gmail.com>
2020-06-10 18:04:09 +0300

    First commit

---
+++ week1.py
@@ -0,0 +1,10 @@
+
+#print("Hello world2!")
+
+#print("hello, " "Aurélien!", "How are you?")
+
+#print(1,"plus", 2, "equals", 1+2)
+
+#name = input("Give me your name: Jarkko")
+#print("Hello,", name)
+
