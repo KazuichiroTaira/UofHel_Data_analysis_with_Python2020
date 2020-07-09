6387c9f564bd20c1617dc3ab561599fd86f22f8c
KazuichiroTaira <ktaira098@gmail.com>
2020-07-09 16:49:43 +0300

    day 15

--- week1.py
+++ week1.py
@@ -7,20 +7,20 @@
 
 #name = input("Give me your name: Jarkko")
 #print("Hello,", name)
-# L1 = [9,1,4,2,5,3,6,8,7,0]
-# L2 = [15,19,17,12,18,16,13,14,11,20]
+
+
+
 
 L1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 L2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-#L = marge_Ls
-#
-def L:
-    L = len(L1) + len(L2)
+
+
+def marge_Ls(L):
+
+    Ls(L) = len(L1) + len(L2)
+
     return L
 
-print(L)
 
-
-#
-# for i in L:
-#     print(i)
+for i in x:
+    print(i)
