6a1dcde7133c3dc8cc34ba4b396285b77f377a53
KazuichiroTaira <ktaira098@gmail.com>
2020-07-13 18:07:30 +0300

    day 16, some contiguous numbers in list, which must describe as (min, max), yet other non-contiguous needs to be appeared as itself.

--- week1.py
+++ week1.py
@@ -8,19 +8,48 @@
 #name = input("Give me your name: Jarkko")
 #print("Hello,", name)
 
+detect_ranges = [2, 5, 4, 8, 12, 6, 7, 10, 13]
+
+i = sorted(detect_ranges)
+
+def detect_ranges(i):
+    return i == list(range(min)(i), max(i)+1)
+
+ith = [2, 5, 4, 8, 12, 6, 7, 10, 13]
+
+print(detect_ranges(ith))
 
 
 
-L1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-L2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# #     (sorted(detect_ranges))    <- range(2, 5) replace
+#                                     ^meaning 3,4,5,6 are replaced by pare of (4,7)
+#                                     Also,
 
 
-def marge_Ls(L):
 
-    Ls(L) = len(L1) + len(L2)
+# my_sorted_list = sorted(detect_ranges)
 
-    return L
+# print(my_sorted_list)
 
 
-for i in x:
-    print(i)
+#
+# for i in range(len(my_sorted_list)):
+#     vi = my_sorted_list[i]
+#
+#     print(i, vi)
+#
+#
+#     if my_sorted_list(vi):
+#
+#         vi
+
+    # if the number index i is number is contiguous to the next number
+    # show the contiguous numbers with only its lower and higher number
+
+    # if i in
+
+    # if the number index i is number is not contiguous to the next number
+
+    # print(i, end=" ")
+
+# print(range(2,5) in (detect_ranges))
